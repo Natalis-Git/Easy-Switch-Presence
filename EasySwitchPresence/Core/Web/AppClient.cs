dd77325ebac2060372d85b508367b1148e2f72d7
Natalis-Git <whovian154@gmail.com>
2021-04-10 22:47:20 -0400

    Added AppClient class file

---
+++ EasySwitchPresence/Core/Web/AppClient.cs
@@ -0,0 +1,15 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+
+
+namespace EasySwitchPresenceWeb
+{
+    public class AppClient
+    {
+        // TODO: Implement application http client for retrieving assets
+    }
+}
